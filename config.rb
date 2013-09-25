4a766026db611a8b99d4696031201b8206166c3d
Timothy Legge <thewizzard@omits-dev.org.uk>
2013-09-25 22:44:52 +0100

    Initial push.

---
+++ config.rb
@@ -0,0 +1,19 @@
+require 'sass-globbing'
+
+# Require any additional compass plugins here.
+project_type = :stand_alone
+
+# Publishing paths
+http_path = "/sub-sector-pandoricus"
+http_images_path = "/sub-sector-pandoricus/images"
+http_generated_images_path = "/sub-sector-pandoricus/images"
+http_fonts_path = "/sub-sector-pandoricus/fonts"
+css_dir = "public/sub-sector-pandoricus/stylesheets"
+
+# Local development paths
+sass_dir = "sass"
+images_dir = "source/images"
+fonts_dir = "source/fonts"
+
+line_comments = false
+output_style = :compressed
